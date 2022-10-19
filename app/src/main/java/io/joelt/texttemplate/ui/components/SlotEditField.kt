6b1a61ec12bbf4bb4d799901bde918b3bcb4cbb7
Joel Tio <joeltiojinhon@gmail.com>
2022-10-19 19:39:14 +0800

    feat: add left and right icon buttons for SlotEditField

--- app/src/main/java/io/joelt/texttemplate/ui/components/SlotEditField.kt
+++ app/src/main/java/io/joelt/texttemplate/ui/components/SlotEditField.kt
@@ -1,8 +1,11 @@
 package io.joelt.texttemplate.ui.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
@@ -30,13 +33,41 @@ fun createSlotText(slot: Slot, selected: Boolean = false): AnnotatedString {
 }
 
 @Composable
-fun SlotEditField(slot: Slot, onChange: (newSlot: Slot) -> Unit) {
-    when (slot) {
-        is PlainTextSlot -> {
-            TextField(value = slot.value, onValueChange = {
-                onChange(slot.copy(value = it))
-            })
+fun SwitchSlotButton(isLeft: Boolean, onClick: (() -> Unit)?) {
+    var icon = Icons.Filled.KeyboardArrowLeft
+    var description = "previous slot"
+    if (!isLeft) {
+        icon = Icons.Filled.KeyboardArrowRight
+        description = "next slot"
+    }
+
+    var tint = Color.Black
+    if (onClick == null) {
+        tint = Color.Gray
+    }
+
+    IconButton(onClick = onClick ?: {}, ) {
+        Icon(icon, description, tint = tint)
+    }
+}
+
+@Composable
+fun SlotEditField(
+    slot: Slot,
+    onLeft: (() -> Unit)? = null,
+    onRight: (() -> Unit)? = null,
+    onChange: (newSlot: Slot) -> Unit
+) {
+    Row {
+        SwitchSlotButton(isLeft = true, onLeft)
+        when (slot) {
+            is PlainTextSlot -> {
+                TextField(value = slot.value, onValueChange = {
+                    onChange(slot.copy(value = it))
+                })
+            }
         }
+        SwitchSlotButton(isLeft = false, onRight)
     }
 }
 
