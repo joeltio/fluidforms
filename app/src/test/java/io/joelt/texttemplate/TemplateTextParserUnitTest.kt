c4e20432f2aed7e2f7c881a6d9c3d4f785a90a2a
Joel Tio <joeltiojinhon@gmail.com>
2022-10-22 18:22:29 +0800

    fix: test used wrong argument for expected result

--- app/src/test/java/io/joelt/texttemplate/TemplateTextParserUnitTest.kt
+++ app/src/test/java/io/joelt/texttemplate/TemplateTextParserUnitTest.kt
@@ -22,56 +22,56 @@ class TemplateTextParserUnitTest {
         val text = "Lorem ipsum dolor sit amet\nLorem ipsum\nLorem Lorem"
         val parsed = text.toTemplateSlot()
 
-        assertEquals(lValue(parsed[0]), text)
+        assertEquals(text, lValue(parsed[0]))
     }
 
     @Test
     fun parse_text_odd_slot_positions() {
         var parsed = "{% text %}xyz{% end %}Lorem ipsum dolor sit amet\nLorem ipsum\nLorem Lorem".toTemplateSlot()
-        assertEquals(parsed.size, 2)
-        assertEquals((rValue(parsed[0]) as PlainTextSlot).value, "xyz")
-        assertEquals(lValue(parsed[1]), "Lorem ipsum dolor sit amet\nLorem ipsum\nLorem Lorem")
+        assertEquals(2, parsed.size)
+        assertEquals("xyz", (rValue(parsed[0]) as PlainTextSlot).value)
+        assertEquals("Lorem ipsum dolor sit amet\nLorem ipsum\nLorem Lorem", lValue(parsed[1]))
 
         parsed = "Lorem ipsum dolor sit amet\nLorem ipsum\nLorem Lorem{% text %}xyz{% end %}".toTemplateSlot()
-        assertEquals(parsed.size, 2)
-        assertEquals(lValue(parsed[0]), "Lorem ipsum dolor sit amet\nLorem ipsum\nLorem Lorem")
-        assertEquals((rValue(parsed[1]) as PlainTextSlot).value, "xyz")
+        assertEquals(2, parsed.size)
+        assertEquals("Lorem ipsum dolor sit amet\nLorem ipsum\nLorem Lorem", lValue(parsed[0]))
+        assertEquals("xyz", (rValue(parsed[1]) as PlainTextSlot).value)
 
 
         parsed = "{% text %}xyz{% end %}".toTemplateSlot()
-        assertEquals(parsed.size, 1)
-        assertEquals((rValue(parsed[0]) as PlainTextSlot).value, "xyz")
+        assertEquals(1, parsed.size)
+        assertEquals("xyz", (rValue(parsed[0]) as PlainTextSlot).value)
     }
 
     @Test
     fun parse_text_with_label() {
         val parsed = "{% text | label=\"the label\" %} hello {% end %}".toTemplateSlot()
-        assertEquals(parsed.size, 1)
-        assertEquals((rValue(parsed[0]) as PlainTextSlot).value, " hello ")
-        assertEquals((rValue(parsed[0]) as PlainTextSlot).label, "the label")
+        assertEquals(1, parsed.size)
+        assertEquals(" hello ", (rValue(parsed[0]) as PlainTextSlot).value)
+        assertEquals("the label", (rValue(parsed[0]) as PlainTextSlot).label)
     }
 
     @Test
     fun parse_text_with_label_trims_whitespace() {
         val parsed = "{%     text   |   label    =   \"  the label \"    %}hello{% end %}".toTemplateSlot()
-        assertEquals(parsed.size, 1)
-        assertEquals((rValue(parsed[0]) as PlainTextSlot).value, "hello")
-        assertEquals((rValue(parsed[0]) as PlainTextSlot).label, "  the label ")
+        assertEquals(1, parsed.size)
+        assertEquals("hello", (rValue(parsed[0]) as PlainTextSlot).value)
+        assertEquals("  the label ", (rValue(parsed[0]) as PlainTextSlot).label)
     }
 
     @Test
     fun parse_text_trims_whitespace_in_tag() {
         val parsed = "{%            text      %}xyz{%end      %}".toTemplateSlot()
-        assertEquals(parsed.size, 1)
-        assertEquals((rValue(parsed[0]) as PlainTextSlot).value, "xyz")
+        assertEquals(1, parsed.size)
+        assertEquals("xyz", (rValue(parsed[0]) as PlainTextSlot).value)
     }
 
     @Test
     fun parse_text_multiple_tags() {
         val parsed = "{% text %} hello {% end %} middle text {% text %} goodbye{% end %}".toTemplateSlot()
-        assertEquals(parsed.size, 3)
-        assertEquals((rValue(parsed[0]) as PlainTextSlot).value, " hello ")
-        assertEquals(lValue(parsed[1]), " middle text ")
-        assertEquals((rValue(parsed[2]) as PlainTextSlot).value, " goodbye")
+        assertEquals(3, parsed.size)
+        assertEquals(" hello ", (rValue(parsed[0]) as PlainTextSlot).value)
+        assertEquals(" middle text ", lValue(parsed[1]))
+        assertEquals(" goodbye", (rValue(parsed[2]) as PlainTextSlot).value)
     }
 }
