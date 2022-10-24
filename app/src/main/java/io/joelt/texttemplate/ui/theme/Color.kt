266d45033fa6fccbe10ee5de17f9008547cf18f3
Joel Tio <joeltiojinhon@gmail.com>
2022-10-24 16:00:44 +0800

    feat: add color scheme

--- app/src/main/java/io/joelt/texttemplate/ui/theme/Color.kt
+++ app/src/main/java/io/joelt/texttemplate/ui/theme/Color.kt
@@ -2,12 +2,13 @@ package io.joelt.texttemplate.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val SlightlyDarkerWhite = Color(0xFFFDFDFD)
+val MuchDarkerWhite = Color(0xFFEFEFEF)
+val TransparentGreyMid = Color(0x7FFFFFFF)
+val DarkBlueLow = Color(0xFF1E212B)
 
-val Yellow = Color(0xFFF5FF3D)
-val Grey = Color(0xFFF7F7F8)
-val DarkBlue = Color(0xFF1E212B)
-val Blue = Color(0xFF0A2463)
+val BlueMid = Color(0xFF2F509A)
+val YellowHigh = Color(0xFFF3FF05)
+val GreyMid = Color(0x1A000000)
--- app/src/main/java/io/joelt/texttemplate/ui/theme/Theme.kt
+++ app/src/main/java/io/joelt/texttemplate/ui/theme/Theme.kt
@@ -1,21 +1,39 @@
 package io.joelt.texttemplate.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColorScheme(
-    primary = Purple200,
-    secondary = Teal200
-)
+val ColorScheme.onPrimarySecondary: Color
+    get() = TransparentGreyMid
+val ColorScheme.highlightEnabled: Color
+    get() = YellowHigh
+val ColorScheme.onHighlightEnabled: Color
+    get() = Black
+val ColorScheme.highlightDisabled: Color
+    get() = GreyMid
+val ColorScheme.onHighlightDisabled: Color
+    get() = Black
 
 private val LightColorPalette = lightColorScheme(
-    primary = DarkBlue,
-    secondary = Yellow,
-    background = Grey
+    primary = BlueMid,
+    onPrimary = White,
+    primaryContainer = White,
+    onPrimaryContainer = Black,
+    tertiary = YellowHigh,
+    onTertiary = Black,
+    tertiaryContainer = DarkBlueLow,
+    onTertiaryContainer = White,
+    surfaceTint = SlightlyDarkerWhite,
+    surface = SlightlyDarkerWhite,
+    background = MuchDarkerWhite,
+    onBackground = Black
 )
+
+private val DarkColorPalette = LightColorPalette
 
 @Composable
 fun TextTemplateTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
