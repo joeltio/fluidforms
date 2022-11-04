def71f1932958f516a212965e9922309bdc23af9
Joel Tio <joeltiojinhon@gmail.com>
2022-11-04 17:32:43 +0800

    feat: add CardsColumn component

--- app/src/main/java/io/joelt/texttemplate/ui/components/CardsColumn.kt
+++ app/src/main/java/io/joelt/texttemplate/ui/components/CardsColumn.kt
@@ -1,2 +1,27 @@
 package io.joelt.texttemplate.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardsColumn(loading: Boolean, block: @Composable () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (loading) {
+            Spacer(Modifier.height(32.dp))
+            CircularProgressIndicator()
+        } else {
+            block()
+        }
+    }
+}
