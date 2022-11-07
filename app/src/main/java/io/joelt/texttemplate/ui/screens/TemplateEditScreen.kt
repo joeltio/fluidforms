d910f8c0d4d3afe5def54642d5e8526913b454db
Joel Tio <joeltiojinhon@gmail.com>
2022-11-07 17:21:52 +0800

    fix: replace Int with Long for ids passed through navarg

--- app/src/main/java/io/joelt/texttemplate/ui/screens/TemplateEditScreen.kt
+++ app/src/main/java/io/joelt/texttemplate/ui/screens/TemplateEditScreen.kt
@@ -13,18 +13,18 @@ class TemplateEditScreen : Screen {
     override fun route(): String = "templates/{templateId}/edit"
 
     override fun arguments(): List<NamedNavArgument> = listOf(
-        navArgument("templateId") { type = NavType.IntType }
+        navArgument("templateId") { type = NavType.LongType }
     )
 
     override fun makeComposable(backStackEntry: NavBackStackEntry): ScreenComposable =
         { nav, scaffold ->
             scaffold.changeNavBars(ScaffoldType.TEMPLATE_EDIT_SCREEN)
-            val templateId = backStackEntry.arguments!!.getInt("templateId")
+            val templateId = backStackEntry.arguments!!.getLong("templateId")
             TemplateEditScreen(nav, scaffold, templateId)
         }
 }
 
 @Composable
-private fun TemplateEditScreen(nav: NavHostController, scaffold: ScaffoldController, templateId: Int) {
+private fun TemplateEditScreen(nav: NavHostController, scaffold: ScaffoldController, templateId: Long) {
     Text(text = "template edit screen for template with id $templateId")
 }
