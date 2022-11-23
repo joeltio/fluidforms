b2cd0ac6822172e467a8f3d78349cd3be3aeeba5
Joel Tio <joeltiojinhon@gmail.com>
2022-11-23 20:09:29 +0800

    feat: add TemplatePreview component

---
+++ app/src/main/java/io/joelt/texttemplate/ui/components/TemplatePreview.kt
@@ -0,0 +1,23 @@
+package io.joelt.texttemplate.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.joelt.texttemplate.models.Template
+import io.joelt.texttemplate.models.genTemplates
+
+@Composable
+fun TemplatePreview(template: Template) {
+    TemplateViewLayout(name = {
+        Text(text = template.name)
+    }) {
+        SlotsPreview(slots = template.slots)
+    }
+}
+
+@Preview
+@Composable
+private fun TemplatePreviewPreview() {
+    val template = genTemplates(1)[0]
+    TemplatePreview(template)
+}
