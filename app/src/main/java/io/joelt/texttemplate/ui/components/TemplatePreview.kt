c731b93fbb074de43f3bae55625169f770f274e6
Joel Tio <joeltiojinhon@gmail.com>
2022-11-24 18:56:41 +0800

    feat: add alternative version of TemplatePreview

--- app/src/main/java/io/joelt/texttemplate/ui/components/TemplatePreview.kt
+++ app/src/main/java/io/joelt/texttemplate/ui/components/TemplatePreview.kt
@@ -4,15 +4,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import io.joelt.texttemplate.models.Either
 import io.joelt.texttemplate.models.Template
 import io.joelt.texttemplate.models.genTemplates
+import io.joelt.texttemplate.models.slots.Slot
 
 @Composable
 fun TemplatePreview(template: Template) {
+    TemplatePreview(name = template.name, slots = template.slots)
+}
+
+@Composable
+fun TemplatePreview(name: String, slots: List<Either<String, Slot>>) {
     TemplateViewLayout(contentScrollable = true, name = {
-        Text(text = template.name, maxLines = 1, overflow = TextOverflow.Ellipsis)
+        Text(text = name, maxLines = 1, overflow = TextOverflow.Ellipsis)
     }) {
-        SlotsPreview(slots = template.slots)
+        SlotsPreview(slots = slots)
     }
 }
 
