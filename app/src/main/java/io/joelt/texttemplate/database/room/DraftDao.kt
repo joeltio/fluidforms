16590ea1f458478691b1f6cafeb7357667e31c7a
Joel Tio <joeltiojinhon@gmail.com>
2022-11-25 20:59:35 +0800

    fix: sort drafts by last_edited_on instead of created_on

--- app/src/main/java/io/joelt/texttemplate/database/room/DraftDao.kt
+++ app/src/main/java/io/joelt/texttemplate/database/room/DraftDao.kt
@@ -1,7 +1,6 @@
 package io.joelt.texttemplate.database.room
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
@@ -11,10 +10,10 @@ interface DraftDao {
     @Insert
     fun insert(draft: Draft): Long
 
-    @Query("SELECT * FROM draft WHERE archived = :archived ORDER BY draft.created_on DESC")
+    @Query("SELECT * FROM draft WHERE archived = :archived ORDER BY draft.last_edited_on DESC")
     fun getDrafts(archived: Boolean): List<Draft>
 
-    @Query("SELECT * FROM draft ORDER BY draft.created_on DESC")
+    @Query("SELECT * FROM draft ORDER BY draft.last_edited_on DESC")
     fun getDrafts(): List<Draft>
 
     @Query("SELECT * FROM draft WHERE id = :id")
