cbb447d5351bcf2ca436c449bb49d17eed1cc6d4
Joel Tio <joeltiojinhon@gmail.com>
2022-12-01 20:48:36 +0800

    refactor: update ArchivedScreen to use new screen DSL

--- app/src/main/java/io/joelt/texttemplate/ui/screens/archived/ArchivedScreen.kt
+++ app/src/main/java/io/joelt/texttemplate/ui/screens/archived/ArchivedScreen.kt
@@ -6,8 +6,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.navigation.NamedNavArgument
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import io.joelt.texttemplate.models.Draft
 import io.joelt.texttemplate.navigation.*
@@ -21,40 +19,38 @@ val Route.archived: String
     get() = "archived"
 
 @OptIn(ExperimentalMaterial3Api::class)
-class ArchivedScreen : Screen {
-    override val route: String = Route.archived
-    override val arguments: List<NamedNavArgument> = emptyList()
-    @Composable
-    override fun scaffold(nav: NavHostController): ScaffoldOptions {
-        val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
-        return ScaffoldOptions(
-            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-            topBar = { TopNavBar(nav, scrollBehavior) },
-            bottomBar = { BottomNavBar(nav) }
-        )
-    }
-
-    @Composable
-    override fun Composable(backStackEntry: NavBackStackEntry, nav: NavHostController) {
-        ArchivedScreen(nav)
-    }
-}
-
 @Composable
-private fun ArchivedScreenContent(archived: List<Draft>?, onArchivedClick: (Draft) -> Unit) {
-    DraftList(drafts = archived, onItemClick = onArchivedClick)
-}
-
-@Composable
-private fun ArchivedScreen(
+private fun archivedScreenContent(
     nav: NavHostController,
-    viewModel: ArchivedViewModel = koinViewModel()
-) {
-    LaunchedEffect(Unit) {
-        viewModel.loadArchived()
+    archived: List<Draft>?,
+    onArchivedClick: (Draft) -> Unit
+) = buildScreenContent {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    scaffoldOptions {
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+        topBar = { TopNavBar(nav, scrollBehavior) }
+        bottomBar = { BottomNavBar(nav) }
     }
 
-    ArchivedScreenContent(archived = viewModel.archived) {
-        nav.navigate(Route.archivedPreview(it.id))
+    content {
+        DraftList(drafts = archived, onItemClick = onArchivedClick)
+    }
+}
+
+val ArchivedScreen = buildScreen {
+    route = Route.archived
+    arguments = emptyList()
+
+    contentFactory { _, nav ->
+        val viewModel: ArchivedViewModel = koinViewModel()
+
+        LaunchedEffect(Unit) {
+            viewModel.loadArchived()
+        }
+
+        archivedScreenContent(
+            nav = nav,
+            archived = viewModel.archived,
+            onArchivedClick = { nav.navigate(Route.archivedPreview(it.id)) })
     }
 }
