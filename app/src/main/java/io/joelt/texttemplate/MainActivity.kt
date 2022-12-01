2fd28822fd3509b4eb62cc332ece8048d5acc4eb
Joel Tio <joeltiojinhon@gmail.com>
2022-12-02 00:31:04 +0800

    refactor: update MainActivity to use new screens

--- app/src/main/java/io/joelt/texttemplate/MainActivity.kt
+++ app/src/main/java/io/joelt/texttemplate/MainActivity.kt
@@ -1,15 +1,11 @@
 package io.joelt.texttemplate
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.*
 import androidx.core.view.WindowCompat
-import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,7 +13,6 @@ import io.joelt.texttemplate.database.LocalPreferences
 import io.joelt.texttemplate.database.UserPreferences
 import io.joelt.texttemplate.database.UserPreferencesRepository
 import io.joelt.texttemplate.navigation.ScaffoldOptions
-import io.joelt.texttemplate.navigation.currentRouteAsState
 import io.joelt.texttemplate.ui.components.SystemBarScaffold
 import io.joelt.texttemplate.ui.screens.archived.ArchivedScreen
 import io.joelt.texttemplate.ui.screens.archived_preview.ArchivedPreviewScreen
@@ -28,13 +23,10 @@ import io.joelt.texttemplate.ui.screens.template_edit.TemplateEditScreen
 import io.joelt.texttemplate.ui.screens.template_preview.TemplatePreviewScreen
 import io.joelt.texttemplate.ui.screens.templates.TemplatesScreen
 import io.joelt.texttemplate.ui.theme.TextTemplateTheme
-
-private val Context.dataStore by preferencesDataStore(
-    name = "user_preferences"
-)
+import org.koin.android.ext.android.inject
 
 @Composable
-fun AppScaffold(scaffoldOptions: ScaffoldOptions, content: @Composable () -> Unit) {
+fun AppScaffold(scaffoldOptions: ScaffoldOptions, content: @Composable BoxScope.() -> Unit) {
     SystemBarScaffold(scaffoldOptions, content)
 }
 
@@ -43,37 +35,37 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
-        val preferencesRepo = UserPreferencesRepository(dataStore)
         setContent {
             // Screens
             val screenStartIndex = 0
             val screens = listOf(
-                TemplatesScreen(),
-                TemplatePreviewScreen(),
-                TemplateEditScreen(),
-                DraftsScreen(),
-                ArchivedScreen(),
-                SettingsScreen(preferencesRepo),
-                DraftEditScreen(),
-                ArchivedPreviewScreen(),
+                TemplatesScreen,
+                TemplatePreviewScreen,
+                TemplateEditScreen,
+                DraftsScreen,
+                ArchivedScreen,
+                SettingsScreen,
+                TemplatesScreen,
+                DraftEditScreen,
+                ArchivedPreviewScreen,
             )
 
             // Navigation
             val navController = rememberNavController()
-            val currentRoute = navController.currentRouteAsState()
-            val screenMap = buildMap { screens.forEach { screen -> put(screen.route, screen) } }
-            val startingScreen = screens[screenStartIndex]
-            val currentScreen = screenMap[currentRoute] ?: startingScreen
+            var scaffoldOptions by remember { mutableStateOf(ScaffoldOptions()) }
 
             // Preferences
+            val preferencesRepo: UserPreferencesRepository by inject()
             val preferences by preferencesRepo.flow.collectAsState(initial = UserPreferences())
             CompositionLocalProvider(LocalPreferences provides preferences) {
                 TextTemplateTheme {
-                    AppScaffold(scaffoldOptions = currentScreen.scaffold(navController)) {
-                        NavHost(navController, startDestination = startingScreen.route) {
+                    AppScaffold(scaffoldOptions = scaffoldOptions) {
+                        NavHost(navController, startDestination = screens[screenStartIndex].route) {
                             screens.forEach { screen ->
-                                composable(screen.route, arguments = screen.arguments) {
-                                    screen.Composable(it, navController)
+                                composable(screen.route, arguments = screen.arguments) { backStackEntry ->
+                                    val screenContent = screen.contentFactory(backStackEntry, navController)
+                                    scaffoldOptions = screenContent.scaffoldOptions
+                                    screenContent.content()
                                 }
                             }
                         }
