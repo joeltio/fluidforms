6252ee3ee83394d3589a33e20ad130e3ed0bd282
Joel Tio <joeltiojinhon@gmail.com>
2022-12-12 17:05:33 +0800

    refactor: separate Slot info from Slot, clearly separate serialized Strings that can be used for display will be prefixed with "display"

--- app/src/main/java/io/joelt/texttemplate/models/slots/Slot.kt
+++ app/src/main/java/io/joelt/texttemplate/models/slots/Slot.kt
@@ -2,21 +2,35 @@ package io.joelt.texttemplate.models.slots
 
 import androidx.compose.runtime.Composable
 
+interface SlotInfo {
+    val serializeTag: EscapedString
+    fun deserialize(
+        serializedLabel: EscapedString,
+        serializedModifiers: Map<EscapedString, EscapedString>,
+        serializedValue: EscapedString
+    ): Slot
+}
+
 abstract class Slot {
-    var label = ""
-    protected abstract fun valueToDisplayString(): String
-    // Returns the placeholder if valueToDisplayString is empty
-    fun toDisplayString(): String {
-        val displayStr = valueToDisplayString()
-        if (displayStr == "") {
-            return label
-        }
-        return displayStr
-    }
+    // Serializing properties
+    abstract val info: SlotInfo
+    abstract fun serializeModifiers(): Map<EscapedString, EscapedString>
+    abstract fun serializeValue(): EscapedString
+
+    // Display properties
+    abstract fun displayValue(): String
+    abstract val displayLabel: String
 
     @Composable
-    abstract fun typeName(): String
-    abstract fun serializeValue(): String
-    abstract fun loadSerializedValue(s: String)
-    abstract fun makeCopy(label: String): Slot
+    abstract fun displaySlotType(): String
+    fun displayDefault(): String {
+        val displayVal = displayValue()
+        if (displayVal.isEmpty()) {
+            return displayLabel
+        }
+        return displayVal
+    }
+
+    // Other properties
+    abstract fun makeCopy(displayLabel: String): Slot
 }
